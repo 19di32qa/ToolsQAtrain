835b09861fd15182598137efad2beaa3106c41c6
19di32qa <87597749+19di32qa@users.noreply.github.com>
2022-11-07 13:40:36 +0200

    Started new project using ToolsQA platform

---
+++ src/test/java/PageObjects/TextBox.java
@@ -0,0 +1,15 @@
+package PageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class TextBox {
+    private WebDriver driver;
+    private By 
+
+
+    public void setDriver(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
