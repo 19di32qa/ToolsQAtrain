485a2c3c1430ca45e7f110d2b15230bad57feb8d
19di32qa <87597749+19di32qa@users.noreply.github.com>
2022-11-10 16:39:37 +0200

    added test for frames

--- src/test/java/PageObjects/NestedFramesPage.java
+++ src/test/java/PageObjects/NestedFramesPage.java
@@ -1,11 +1,19 @@
 package PageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class NestedFramesPage {
     WebDriver driver;
     String url = "https://demoqa.com/nestedframes";
     public NestedFramesPage(WebDriver driver) {
         this.driver = driver;
+    }
+    public void getPage() {
+        driver.get(url);
+    }
+    public WebElement getFrame(String selector) {
+        return driver.findElement(By.id(selector));
     }
 }
--- src/test/java/TestCases/TC_0015_NestedFrames.java
+++ src/test/java/TestCases/TC_0015_NestedFrames.java
@@ -1,4 +1,29 @@
 package TestCases;
 
-public class TC_0015_NestedFrames {
+import PageObjects.BaseClass;
+import PageObjects.NestedFramesPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_0015_NestedFrames extends BaseClass {
+
+    @Test
+    public void NestedFramesTest() {
+        WebDriver driver = new ChromeDriver();
+        NestedFramesPage nf = new NestedFramesPage(driver);
+        nf.getPage();
+        WebElement frame = nf.getFrame("frame1");
+        driver.switchTo().frame(frame);
+        Assert.assertEquals(driver.findElement(By.tagName("body")).getText(),"Parent frame");
+        frame = driver.findElement(By.cssSelector("[srcdoc=\"<p>Child Iframe</p>\"]"));
+        driver.switchTo().frame(frame);
+        Assert.assertEquals(driver.findElement(By.tagName("p")).getText(),"Child Iframe");
+        driver.quit();
+
+    }
+
 }
